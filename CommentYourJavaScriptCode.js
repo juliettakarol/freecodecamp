06034e0016f7cead1fb3cf43074c36f9ca9ed368
Julyetta Karol <julietta_karol@mail.ru>
2024-01-30 23:43:01 +0300

    added new freecodecamp task

---
+++ CommentYourJavaScriptCode.js
@@ -0,0 +1,21 @@
+// NOTE: As you write code, you should regularly add comments 
+// to clarify the function of parts of your code.Good commenting 
+// can help communicate the intent of your code—both for others 
+// and for your future self.
+
+
+
+
+// function min(a,b) {
+//     if (a<b) {
+//         return a
+//     }
+//         return b
+// }
+
+/* function min(a,b) {
+    if (a<b) {
+        return a
+    }
+        return b
+}*/
