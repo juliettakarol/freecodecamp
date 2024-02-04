89d8cc0a431f3d66655b950f65a4fb05f3b5bc0d
Julyetta Karol <julietta_karol@mail.ru>
2024-02-04 15:31:11 +0300

    change comment

--- Explore-Differences-Between-the-var-and-let-Keywords.js
+++ Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -1,7 +1,7 @@
 // Update the code so it only uses the let keyword.
 
-var catName = "Oliver";
-var catSound = "Meow!";
+// var catName = "Oliver";
+// var catSound = "Meow!";
 
 let catName = "Oliver";
 let catSound = "Meow!";
