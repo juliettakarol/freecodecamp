3710dd0d9e885dfbd238768d1c17f07be724a5d8
Julyetta Karol <julietta_karol@mail.ru>
2024-02-10 15:00:52 +0300

    added new task

---
+++ Modify-Array-Data-With-Indexes.js
@@ -0,0 +1,10 @@
+// Note: There shouldn't be any spaces between 
+// the array name and the square brackets, like array [0]. 
+// Although JavaScript is able to process this correctly, 
+// this may confuse other programmers reading your code.
+
+// Setup
+const myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[0] = 45 
