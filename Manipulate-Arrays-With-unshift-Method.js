18c958ca7832a8fddff0f64c0817749690b3de5b
Julyetta Karol <julietta_karol@mail.ru>
2024-02-11 23:28:12 +0300

    added new task

---
+++ Manipulate-Arrays-With-unshift-Method.js
@@ -0,0 +1,10 @@
+// Add ["Paul", 35] to the beginning of the 
+// myArray variable using unshift().
+// myArray should now have [["Paul", 35], ["dog", 3]].
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(["Paul", 35])
