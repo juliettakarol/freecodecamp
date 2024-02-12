8f76fb65a224cfd6a6fbc4b4de04e676ed235a66
Julyetta Karol <julietta_karol@mail.ru>
2024-02-12 15:12:21 +0300

    added new task

---
+++ Return-a-Value-from-a-Function-with-Return.js
@@ -0,0 +1,10 @@
+// Create a function timesFive that accepts one 
+// argument, multiplies it by 5, and returns the new value.
+
+function timesFive(num){
+    return num*5
+    }
+    
+    let numbers = timesFive(5)
+    let numbers1 = timesFive(2)
+    let numbers2 = timesFive(0)
