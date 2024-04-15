46357c33e691821f2139054b3feaac6de1536635
Julyetta Karol <julietta_karol@mail.ru>
2024-04-15 23:34:43 +0300

    added

--- BasicDataStructures/Access-Property-Names-with-Bracket-Notation.js
+++ BasicDataStructures/Access-Property-Names-with-Bracket-Notation.js
@@ -15,6 +15,7 @@ let foods = {
   function checkInventory(scannedItem) {
     // Only change code below this line
     return foods[scannedItem]
+    //
     // Only change code above this line
   }
   
