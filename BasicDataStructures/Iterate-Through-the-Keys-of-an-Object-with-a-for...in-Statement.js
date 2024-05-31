21e256e4290762ed19770a6264594e6ff27ac48e
Julyetta Karol <julietta_karol@mail.ru>
2024-05-31 23:55:47 +0300

    added new task

---
+++ BasicDataStructures/Iterate-Through-the-Keys-of-an-Object-with-a-for...in-Statement.js
@@ -0,0 +1,27 @@
+// We've defined a function countOnline which accepts 
+// one argument, allUsers. Use a for...in statement inside
+//  this function to loop through the allUsers object and return 
+//  the number of users whose online property is set to true. An 
+//  example of an object which could be passed to countOnline is
+//   shown below. Each user will have an online property set to either true or false.
+
+
+const users = {
+    Alan: {
+      online: false
+    },
+    Jeff: {
+      online: true
+    },
+    Sarah: {
+      online: false
+    }
+  }
+  
+  function countOnline(allUsers) {
+    // Only change code below this line
+  
+    // Only change code above this line
+  }
+  
+  console.log(countOnline(users));
